489b07aa318f377e9d9a3b3f9204287c1ea6aecc
alieryaman <postauyelik@gmail.com>
2020-04-04 14:22:31 +0300

    tablo ismidegstirmeveri kkaybı olmadan

--- CodeFirst/Migrations/202004041115286_Categoriaadcolondegisirmead.cs
+++ CodeFirst/Migrations/202004041115286_Categoriaadcolondegisirmead.cs
@@ -1,1 +1,22 @@
-﻿
+﻿namespace CodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class Categoriaadcolondegisirmead : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Kategoris", "KategoriAd", c => c.String());
+            Sql("Update Kategoris set KategoriAd=Kategorisim");
+            DropColumn("dbo.Kategoris", "Kategorisim");
+        }
+
+        public override void Down()
+        {
+            AddColumn("dbo.Kategoris", "Kategorisim", c => c.String());
+            Sql("Update Kategoris set Kategorisim=KategoriAd");
+            DropColumn("dbo.Kategoris", "KategoriAd");
+        }
+    }
+}
